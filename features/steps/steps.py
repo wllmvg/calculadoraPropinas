0e010e77544d9370a1651e1f2ff3b9b4afef061a
LAPTOP-VMPCVG2R\Paul Mateo C <paulmateo06@gmail.com>
2025-04-25 10:17:13 -0500

    correccion de error

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+
 from behave import given, when, then
 from tests.calculadora import calcular_propina
 
